caddb0719066d785edad453e11b9f58241fcc296
Alessandra Gomez <94123165+alessgomez@users.noreply.github.com>
2023-04-17 02:09:54 +0800

    Update RecoveryTable.sql

--- public/RecoveryTable.sql
+++ public/RecoveryTable.sql
@@ -1,4 +1,26 @@
-CREATE TABLE `log` (
+CREATE TABLE `recovery0` (
+  `log_id` int NOT NULL AUTO_INCREMENT,
+  `transaction_no` int DEFAULT NULL,
+  `row_no` int DEFAULT NULL,
+  `col_name` varchar(45) DEFAULT NULL,
+  `old_value` varchar(100) DEFAULT NULL,
+  `new_value` varchar(100) DEFAULT NULL,
+  `query` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `recovery1` (
+  `log_id` int NOT NULL AUTO_INCREMENT,
+  `transaction_no` int DEFAULT NULL,
+  `row_no` int DEFAULT NULL,
+  `col_name` varchar(45) DEFAULT NULL,
+  `old_value` varchar(100) DEFAULT NULL,
+  `new_value` varchar(100) DEFAULT NULL,
+  `query` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `recovery2` (
   `log_id` int NOT NULL AUTO_INCREMENT,
   `transaction_no` int DEFAULT NULL,
   `row_no` int DEFAULT NULL,
