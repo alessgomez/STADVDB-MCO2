0e7e031594ca10a869b3ac94a024e459117901cf
Alessandra Gomez <94123165+alessgomez@users.noreply.github.com>
2023-04-18 03:17:40 +0800

    Update MCO2TableCreation.sql

--- public/MCO2TableCreation.sql
+++ public/MCO2TableCreation.sql
@@ -1,33 +1,11 @@
 CREATE TABLE `movies` (
   `id` int NOT NULL DEFAULT '0',
-  `title` varchar(100) CHARACTER SET utf8mb3 DEFAULT NULL,
+  `title` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `year` int DEFAULT NULL,
   `rating` float DEFAULT NULL,
-  `genre` varchar(100) CHARACTER SET utf8mb3 NOT NULL,
-  `director` varchar(201) CHARACTER SET utf8mb3 DEFAULT NULL,
-  `actor` varchar(201) CHARACTER SET utf8mb3 DEFAULT NULL,
+  `genre` varchar(100) DEFAULT NULL,
+  `director` varchar(201) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `actor` varchar(201) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `lastUpdated` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `newmovie` (
-  `id` int NOT NULL DEFAULT '0',
-  `title` varchar(100) CHARACTER SET utf8mb3 DEFAULT NULL,
-  `year` int DEFAULT NULL,
-  `rating` float DEFAULT NULL,
-  `genre` varchar(100) CHARACTER SET utf8mb3 NOT NULL,
-  `director` varchar(201) CHARACTER SET utf8mb3 DEFAULT NULL,
-  `actor` varchar(201) CHARACTER SET utf8mb3 DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `oldmovie` (
-  `id` int NOT NULL DEFAULT '0',
-  `title` varchar(100) CHARACTER SET utf8mb3 DEFAULT NULL,
-  `year` int DEFAULT NULL,
-  `rating` float DEFAULT NULL,
-  `genre` varchar(100) CHARACTER SET utf8mb3 NOT NULL,
-  `director` varchar(201) CHARACTER SET utf8mb3 DEFAULT NULL,
-  `actor` varchar(201) CHARACTER SET utf8mb3 DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
